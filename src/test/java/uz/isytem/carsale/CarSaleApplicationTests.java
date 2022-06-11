256ac55ece84de04ba22745040a2a5dac8926461
asadbektuit <davlatovasadbek1@gmail.com>
2022-06-11 20:47:58 +0500

    Initial commit

---
+++ src/test/java/uz/isytem/carsale/CarSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.isytem.carsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
